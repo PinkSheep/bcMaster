53216b2a7752bd18d0e1517c656df6b7ff6adeb1
Michael Kressibucher <mkressibucher@hotmail.com>
2013-12-18 18:50:45 +0100

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,11 @@
 ENV["RAILS_ENV"] ||= "test"
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
 
 # Coverallas - Test Coverage
 require 'coveralls'
 Coveralls.wear!('rails')
+
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
