ac29d95db098a8c95b3b6bd8f81bcd9281abf67f
kressi <mkressibucher@hotmail.com>
2013-12-19 02:52:49 +0100

    added currencies

--- app/models/btce/api.rb
+++ app/models/btce/api.rb
@@ -1,5 +1,11 @@
 module Btce
   class API
     DOMAIN = "btc-e.com"
+    CURRENCY_PAIRS = %w(
+      btc_usd btc_eur btc_rur eur_usd ftc_btc
+      ltc_btc ltc_eur ltc_rur ltc_usd nmc_btc
+      nmc_usd nvc_btc nvc_usd ppc_btc ppc_usd
+      trc_btc usd_rur xpm_btc)
+    CURRENCIES = CURRENCY_PAIRS.map {|pair| pair.split("_")}.flatten.uniq.sort
   end
 end
