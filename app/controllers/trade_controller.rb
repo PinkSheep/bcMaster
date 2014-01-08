97af3338b38313807ce4f0ec0df3f4193e5f18ed
Michael Kressibucher <mkressibucher@hotmail.com>
2014-01-08 15:58:48 +0100

    made trade_controller test pass

--- app/controllers/trade_controller.rb
+++ app/controllers/trade_controller.rb
@@ -9,8 +9,7 @@ class TradeController < ApplicationController
   	#falls keine Session vorhanden ist, wird man wieder auf die Mainseite weitergeleitet
   	# zu implementieren: notice message, welche dann auf der main-page angezeigt wird
   	@key_array = Array.new
-  	@key_array = session[:bcmaster]
-  	if @key_array.nil?
+  	if (@key_array = session[:bcmaster]).nil?
   		redirect_to main_index_path
   	end
   end
