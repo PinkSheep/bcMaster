46efe623b2e3528509909c5fbefb0b8b6543f3e0
kressi <mkressibucher@hotmail.com>
2014-01-08 23:21:02 +0100

    functional tests moved to controller tests

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 
 class MainControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
   test "should get login" do
     get :login
     assert_response :success
   end
-
 end
