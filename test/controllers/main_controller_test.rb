0e3c3db54e4a1acc1d91a0c03be6cc4c7aa99988
Michael Kressibucher <mkressibucher@hotmail.com>
2014-01-09 10:33:28 +0100

    added test for failing login

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -5,6 +5,10 @@ class MainControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
+  test "failing login should redirect to main" do
+    post :login
+    assert_redirected_to main_index_path
+  end
   test "should redirect to main" do
     get :logout
     assert_redirected_to main_index_path
