3cfcc6e1fa23881f6954782e3d2c3386afb9e154
Michael Kressibucher <mkressibucher@hotmail.com>
2014-01-10 12:39:23 +0100

    temp config.assets.compile = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,9 @@ BcMaster::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+# ! set back to false when
+# ! figured out how to make it work
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
