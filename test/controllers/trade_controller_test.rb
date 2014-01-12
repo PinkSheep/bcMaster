5d1487bdf134eef1c59788b6fa90f7a09fac8030
kressi <mkressibucher@hotmail.com>
2014-01-12 12:24:34 +0100

    tests added for all methods

--- test/controllers/trade_controller_test.rb
+++ test/controllers/trade_controller_test.rb
@@ -1,20 +1,24 @@
 require 'test_helper'
 
 class TradeControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "should get method" do
     session[:bcmaster] = {
       apikey: "YACSECCW-SXC6EV2I-CF679EYB-PQ3NKI93-PENNC1UM",
       secret: "7876863e49c7ba7ac9dba285e9e6313e4b601aecfaab03b6c9704efa56f8f6de" }
-    get :index
-    assert_response :success
+    %w(open_orders transaction_history trade_history index).each do
+      |method| get method
+      assert_response :success
+    end
   end
-  test "should get index and flash error" do
+  test "should flash error" do
     session[:bcmaster] = {
       apikey: "apikey",
       secret: "secret" }
-    get :index
-    assert flash[:error]
-    assert_response :success
+    %w(open_orders transaction_history trade_history index).each do
+      |method| get method
+      assert flash[:error]
+      assert_response :success
+    end
   end
   test "should redirect to main" do
     get :index
