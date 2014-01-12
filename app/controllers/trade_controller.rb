8280b8004793d0b6d9a670aaa09f504e5aa683a7
kressi <mkressibucher@hotmail.com>
2014-01-12 11:05:56 +0100

    draft of transaction and order history added

--- app/controllers/trade_controller.rb
+++ app/controllers/trade_controller.rb
@@ -7,6 +7,7 @@ class TradeController < ApplicationController
     else
       flash[:error] = (!info.nil? && info["error"]) || "something failed"
     end
+
     @orders = {}
     @orders[:columns] = [
       { name: "pair", display_name: "Pair" },
@@ -29,6 +30,54 @@ class TradeController < ApplicationController
     else
       flash[:error] = (!orders.nil? && orders["error"]) || "something failed"
     end
+    
+    @transactions = {}
+    @transactions[:columns] = [
+      { name: "type", display_name: "Type" },
+      { name: "amount", display_name: "Amount" },
+      { name: "currency", display_name: "Currency" },
+      { name: "desc", display_name: "Description" },
+      { name: "status", display_name: "Status" },
+      { name: "timestamp", display_name: "Timestamp"},
+      { name: "id", display_name: "id" }
+    ]
+    if !(transactions = @trade.TransHistory).nil? && transactions["success"] == 1
+      transactions = transactions["return"]
+      trans_collection = []
+      transactions.each_pair{
+        |pair| pair[1]["id"] = pair[0]
+        pair[1]["timestamp"] = Time.at(pair[1]["timestamp"])
+        trans_collection<<pair[1]
+      }
+      @transactions[:collection] = trans_collection
+    else
+      flash[:error] = (!transactions.nil? && transactions["error"]) || "something failed"
+    end
+    
+    @trades = {}
+    @trades[:columns] = [
+      { name: "pair", display_name: "Pair" },
+      { name: "type", display_name: "Type" },
+      { name: "amount", display_name: "Amount" },
+      { name: "rate", display_name: "Rate" },
+      { name: "order_id", display_name: "OrderID" },
+      { name: "is_your_order", display_name: "your order" },
+      { name: "timestamp", display_name: "Timestamp"},
+      { name: "id", display_name: "id" }
+    ]
+    if !(trades = @trade.TradeHistory).nil? && trades["success"] == 1
+      trades = trades["return"]
+      trades_collection = []
+      trades.each_pair{
+        |pair| pair[1]["id"] = pair[0]
+        pair[1]["timestamp"] = Time.at(pair[1]["timestamp"])
+        trades_collection<<pair[1]
+      }
+      @trades[:collection] = trades_collection
+    else
+      flash[:error] = (!trades.nil? && trades["error"]) || "something failed"
+    end
+
   end
 
   private
